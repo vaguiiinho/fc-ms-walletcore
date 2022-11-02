8bb59a6b8921e442f2507b3f14f28fb6ed161f0a
vaguiiinho <avanlendario@gmail.com>
2022-11-02 15:16:04 -0300

    CreateClientUseCase, CreateAccountUseCase

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -19,3 +19,11 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
 }
+
+func TestAddAccountToClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	err := client.AddAccount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
